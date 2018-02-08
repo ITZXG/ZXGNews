3e812515db54927a99ee4d185e96c2b986ee4e5d
xiaogang.zhang <15635395802@163.com>
2018-02-08 17:44:09 +0800

    添加新闻的详情点击事件

---
+++ ZXGNews/Classes/Reply/Controller/ReplyViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReplyViewController.h
+//  ZXGNews
+//
+//  Created by xiaogang.zhang on 2018/2/8.
+//  Copyright © 2018年 Raiing Medical. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReplyViewController : UIViewController
+
+@end
---
+++ ZXGNews/Classes/Reply/Controller/ReplyViewController.m
@@ -0,0 +1,37 @@
+//
+//  ReplyViewController.m
+//  ZXGNews
+//
+//  Created by xiaogang.zhang on 2018/2/8.
+//  Copyright © 2018年 Raiing Medical. All rights reserved.
+//
+
+#import "ReplyViewController.h"
+
+@interface ReplyViewController ()
+
+@end
+
+@implementation ReplyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
